73307c12fb34a0369180130878bc27640007a8ae
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2022-10-04 06:27:24 +0300

    Day one complete

---
+++ pages/index.tsx
@@ -0,0 +1,96 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import requests from '../utils/requests'
+import { Movie } from '../typings'
+import Row from '../components/Row'
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+
+const Home = ({  
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,}:Props) => {
+  return (
+    // <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh] ">
+
+      <Head>
+
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header/>
+      <main className='relative pl-4 pb-4 lg:space-y-4 lg:pl-16'>
+        <Banner netflixOriginals={netflixOriginals}/>
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
+          </section>
+
+      </main>
+
+
+    </div>
+  )
+}
+
+export default Home
+
+export const getServerSideProps = async () =>{
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  ] = await Promise.all([
+    fetch(requests.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(requests.fetchTrending).then((res) => res.json()),
+    fetch(requests.fetchTopRated).then((res) => res.json()),
+    fetch(requests.fetchActionMovies).then((res) => res.json()),
+    fetch(requests.fetchComedyMovies).then((res) => res.json()),
+    fetch(requests.fetchHorrorMovies).then((res) => res.json()),
+    fetch(requests.fetchRomanceMovies).then((res) => res.json()),
+    fetch(requests.fetchDocumentaries).then((res) => res.json()),
+  ])
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results,
+    },
+  }
+
+}
