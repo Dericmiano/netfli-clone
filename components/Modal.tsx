863fdcf483112188a2315459f93b22ef9ca1ec39
Deric Mathenge <deric.mathenge@savannahinformatics.com>
2022-10-06 06:30:34 +0300

    day two

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { XIcon } from '@heroicons/react/solid'
+import { PlusIcon, ThumbUpIcon, VolumeOffIcon, VolumeUpIcon, XIcon } from '@heroicons/react/solid'
 import MuiModal from '@mui/material/Modal'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import {modalState, movieState} from '../atoms/ModalAtom'
@@ -7,14 +7,14 @@ import { useState } from 'react'
 import { Genre, Movie } from '../typings'
 import { Element } from '../typings'
 import ReactPlayer from 'react-player'
-
+import { FaPlay } from 'react-icons/fa'
 // import Modal from '../components/Modal'
 
 const Modal = () => {
   const [showModel, setShowModel] = useRecoilState(modalState)
   const [movie, setMovie] = useRecoilState(movieState)
   const [trailer, setTrailer] = useState("")
-  const [genres, setGenres] = useState<Genre[]>()
+  const [genres, setGenres] = useState<Genre[]>([])
   const [muted, setMuted] = useState(false)
 
   useEffect(()=>{
@@ -45,7 +45,9 @@ const Modal = () => {
   }
 
   return (
-    <MuiModal open={showModel} onClose={handleClose}>
+    <MuiModal open={showModel} onClose={handleClose} className="fixex !top-7 left-0
+     right-0 z-50 mx-auto w-full max-w-5xl overflow-hidden overflow-y-scroll rounded-md
+      scrollbar-hide ">
       <>
       <button onClick={handleClose} className="modalButton absolute right-5 top-5
        !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
@@ -62,7 +64,59 @@ const Modal = () => {
             playing
             muted={muted}
           />
+          <div className='absolute bottom-10 flex w-ful  items-centre justify-between px-10'> 
+            <button className=' flex items-center gap-x-2 rounded bg-white px-8 text-xl
+             font-bold text-black transition hover:bg-[#e6e6e6]'>
+            <FaPlay className='h-7 w-7 text-black'/>
+            Play
 
+              
+            </button>
+            <button className='modalButton'>
+              <PlusIcon  className='h-7 w-7'/>
+            </button>
+            <button className='modalButton'>
+              <ThumbUpIcon className='h-7 w-7' />
+            </button>
+          <button className='modalButton' onClick={()=>setMuted(!muted)}>
+          {muted ? (
+            <VolumeOffIcon className='h-6 w-6' />
+          ):(
+            <VolumeUpIcon className='h-6 w-6' />
+          )}
+          </button>
+          </div>
+
+
+      </div>
+      <div className='flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8'>
+      <div className='space-y-6 text-lg'>
+        <div className='flex items-center space-x-2 text-sm'>
+          <p className='font-semibold text-green-400'>{movie!.vote_average * 10}%match</p>
+          <p className='font-light'>{movie?.release_date || movie?.first_air_date}</p>
+          <div className='flex h-4 items-centre justify-center
+           rounded border border-white/40 px-1.5 text-xs'>
+            HD
+          </div>
+        </div>
+        <div className='flex flex-col gap-x-10 gap-y-10 font-light md:flex-row
+          '>
+          <p className='w-5/6'>{movie?.overview}</p>
+          <div className='flex flex-col space-y-3 text-sm'>
+            <span className='text-[gray]'>
+              Genre:
+            </span>
+            {genres.map((genre)=> genre.name).join(',')}
+          </div>
+          <div>
+            <span className='text-[gray]'>Origina Language: </span>
+
+              {movie?.original_language}
+          </div>
+          <div className='text-[gray]'>Total Votes </div>
+          {movie?.vote_count}
+        </div>
+      </div>
       </div>
       </>
 
